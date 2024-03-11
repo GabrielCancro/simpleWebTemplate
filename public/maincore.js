a4bd7d355c9fec9684201bb3332957a58cf6554b
GabrielCancro3OGS <119443886+GabrielCancro3OGS@users.noreply.github.com>
2024-03-11 15:20:52 -0300

    update

---
+++ public/maincore.js
@@ -0,0 +1,12 @@
+console.log("ASDASDASD");
+
+function load_panel(idnode,panel){
+	$.get('/panels/'+panel+'.html',{'_':$.now()})
+	.done(function(data){
+	    $("#"+idnode).html(data)
+	}).fail(function(jqXHR, textStatus){
+		$("#"+idnode).html("fail load..")
+	});
+}
+
+load_panel("MainContent","home");
